00ca38538210bde9b66bde035f04033e1b422615
Mark <markkamamia@gmail.com>
2024-04-03 12:50:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "DENNIS PRITT is the champion";
+
+document.body.appendChild(newHeader);
